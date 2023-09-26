59631f6727fa0a31655d3ada680fa56e06a0162a
carlos.gutfer <carlos.gutfer@hotmail.es>
2023-09-26 08:48:45 +0200

    x787

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = create_app()
 
 @app.route('/mostrar_imagen/<nombre_archivo>')
 def mostrar_imagen(nombre_archivo):
-    return send_from_directory('images', nombre_archivo)
+    return send_from_directory('./web_atletismo/webside/images', nombre_archivo)
 
 if __name__ == '__main__':    
     app.run(debug= True)
